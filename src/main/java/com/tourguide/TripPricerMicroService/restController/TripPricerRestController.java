921f9f8e86b8d618042358b74c964352676df3a4
OrionBS <work@orionbs.fr>
2021-12-18 17:57:29 +0100

    add javaDoc

--- src/main/java/com/tourguide/TripPricerMicroService/restController/TripPricerRestController.java
+++ src/main/java/com/tourguide/TripPricerMicroService/restController/TripPricerRestController.java
@@ -18,11 +18,27 @@ public class TripPricerRestController {
         this.tripPricerService = tripPricerService;
     }
 
+    /**
+     * Transmet une liste de Provider avec un prix en fonction des params.
+     * @param apiKey la clé de l'API.
+     * @param attractionId l'id de l'attraction.
+     * @param adults le nombre d'adultes.
+     * @param children le nombre d'enfants.
+     * @param nightsStay le nombre de nuits du séjour.
+     * @param rewardsPoints le nombre de points gagnés.
+     * @return la liste de Provider personnalisée.
+     */
     @GetMapping(path = "/getPrice")
     public List<Provider> getPrice(@RequestParam String apiKey, @RequestParam UUID attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
         return tripPricerService.getPrice(apiKey, attractionId, adults, children, nightsStay, rewardsPoints);
     }
 
+    /**
+     * Transmet le nom du Provider en fonction des params.
+     * @param apiKey la clé de l'API.
+     * @param adults le nombre d'adultes.
+     * @return le nom du Provider correspondant.
+     */
     @GetMapping(path = "/getProviderName")
     public String getProviderName(@RequestParam String apiKey, @RequestParam int adults) {
         return tripPricerService.getProviderName(apiKey, adults);
--- src/main/java/com/tourguide/TripPricerMicroService/service/TripPricerService.java
+++ src/main/java/com/tourguide/TripPricerMicroService/service/TripPricerService.java
@@ -7,7 +7,23 @@ import java.util.UUID;
 
 public interface TripPricerService {
 
+    /**
+     * Récupère une liste de Provider avec un prix en fonction des params.
+     * @param apiKey la clé de l'API.
+     * @param attractionId l'id de l'attraction.
+     * @param adults le nombre d'adultes.
+     * @param children le nombre d'enfants.
+     * @param nightsStay le nombre de nuits du séjour.
+     * @param rewardsPoints le nombre de points gagnés.
+     * @return la liste de Provider personnalisée.
+     */
     public List<Provider> getPrice(String apiKey, UUID attractionId, int adults, int children, int nightsStay, int rewardsPoints);
 
+    /**
+     * Récupère le nom du Provider en fonction des params.
+     * @param apiKey la clé de l'API.
+     * @param adults le nombre d'adultes.
+     * @return le nom du Provider correspondant.
+     */
     public String getProviderName(String apiKey, int adults);
 }
